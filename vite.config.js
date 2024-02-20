bcdbbef5913ca463ed0ac68f9f5b181ffc9b04b7
Teuku Ridho Abdullah <teukuridho@MacBook-Pro.local>
2024-02-21 00:47:17 +0700

    some optimization

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,16 @@ export default defineConfig(({mode}) => {
         ],
         build: {
             cssCodeSplit: true,
-            lib: false
+            lib: false,
+            rollupOptions: {
+                output:{
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                        }
+                    } // https://stackoverflow.com/questions/69260715/skipping-larger-chunks-while-running-npm-run-build
+                }
+            }
         },
         base: mode == "production" ? env.VITE_BASE + "/build" : null,
     }
