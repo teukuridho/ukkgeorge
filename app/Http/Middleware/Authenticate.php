70599e56bf7a000117fe936b6387ce8aaae03a62
Teuku Ridho Abdullah <teukuridho@MacBook-Pro.local>
2024-02-21 01:08:06 +0700

    modify auth middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,13 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('auth.login');
+        if($request->expectsJson()) {
+            return response()->json([
+                "status" => false,
+                "text" => "Perlu login!"
+            ]);
+        }
+
+        return route('auth.login');
     }
 }
